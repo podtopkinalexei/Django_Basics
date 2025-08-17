fb13374a786629ba02ee83c479725bd4c0324eac
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-08-17 11:03:33 +0300

    Шаблоны для приложкния catalog перенесены в папку catalog

--- Django_Basics/urls.py
+++ Django_Basics/urls.py
@@ -1,9 +1,16 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from blog.admin import blog_admin_site
 
 urlpatterns = [
+    path('admin/blog/', blog_admin_site.urls),
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('blog/', include('blog.urls', namespace='blog')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
