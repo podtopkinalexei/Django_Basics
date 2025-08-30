1c3703dacaa0dab4045fab8d023b62554eb488b9
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-08-30 19:31:07 +0300

    Установлено ограничение для просмотра страниц не авторизованных пользователей.

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import ProductForm
-from .models import Product, Category
+from catalog.forms import ProductForm
+from catalog.models import Product, Category
 
 
 class IndexView(ListView):
@@ -31,28 +32,28 @@ class CatalogView(ListView):
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'pk'
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:catalog')
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:catalog')
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:catalog')
