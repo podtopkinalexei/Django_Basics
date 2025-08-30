1c9c53d532e02d211065503c36d1b9b73f3e6de3
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-08-30 19:29:13 +0300

    Созданы шаблоны для входа, регистрации и управления профилем пользователя

--- Django_Basics/urls.py
+++ Django_Basics/urls.py
@@ -8,6 +8,7 @@ from blog.admin import blog_admin_site
 urlpatterns = [
     path('admin/blog/', blog_admin_site.urls),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
